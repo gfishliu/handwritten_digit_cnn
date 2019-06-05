c908803f093dda6ff1b686d6f961e37aecbac266
gentlefishliu <gentlefishliu@126.com>
2019-06-05 10:33:11 +0900

    cnn init

--- keras-cnn.py
+++ keras-cnn.py
@@ -1,72 +1,59 @@
-'''Trains a simple convnet on the MNIST dataset.
-Gets to 99.25% test accuracy after 12 epochs
-(there is still a lot of margin for parameter tuning).
-16 seconds per epoch on a GRID K520 GPU.
-'''
-#http://nooverfit.com/wp/keras-%E6%89%8B%E6%8A%8A%E6%89%8B%E5%85%A5%E9%97%A81-%E6%89%8B%E5%86%99%E6%95%B0%E5%AD%97%E8%AF%86%E5%88%AB-%E6%B7%B1%E5%BA%A6%E5%AD%A6%E4%B9%A0%E5%AE%9E%E6%88%98/
-
-from __future__ import print_function
 import keras
 from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-from keras import backend as K
+from keras.layers import Dense, Flatten
+from keras.models import Sequential
+'''
+LeNet-5 
+2018/7/15
+ThomasZou
 
-batch_size = 128
-num_classes = 10
-epochs = 8
-
-# input image dimensions
-img_rows, img_cols = 28, 28
-
-# the data, shuffled and split between train and test sets
+'''
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+# 输入数据为 mnist 数据集
+x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
+x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+x_train = x_train / 255
+x_test = x_test / 255
 
-# convert class vectors to binary class matrices
-y_train = keras.utils.np_utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.np_utils.to_categorical(y_test, num_classes)
+y_train = keras.utils.to_categorical(y_train, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
 
 model = Sequential()
-model.add(Conv2D(32,
-                 activation='relu',
-                 input_shape=input_shape,
-                 nb_row=3,
-                 nb_col=3))
-model.add(Conv2D(64, activation='relu',
-                 nb_row=3,
-                 nb_col=3))
-# model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# 选取6个特征卷积核，大小为5∗5(不包含偏置),得到66个特征图，每个特征图的大小为32−5+1=2832−5+1=28，
+# 也就是神经元的个数由10241024减小到了28∗28=78428∗28=784。
+# 输入层与C1层之间的参数:6∗(5∗5+1)
+model.add(Conv2D(6, kernel_size=(5, 5), activation='relu', input_shape=(28, 28, 1)))
+
+# 这一层的输入为第一层的输出，是一个28*28*6的节点矩阵。
+# 本层采用的过滤器大小为2*2，长和宽的步长均为2，所以本层的输出矩阵大小为14*14*6。
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.35))
+
+# 本层的输入矩阵大小为14*14*6，使用的过滤器大小为5*5，深度为16.本层不使用全0填充，步长为1。
+# 本层的输出矩阵大小为10*10*16。本层有5*5*6*16+16=2416个参数
+model.add(Conv2D(16, kernel_size=(5, 5), activation='relu'))
+
+# 本层的输入矩阵大小10*10*16。本层采用的过滤器大小为2*2，长和宽的步长均为2，所以本层的输出矩阵大小为5*5*16。
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+# 本层的输入矩阵大小为5*5*16，在LeNet-5论文中将这一层称为卷积层，但是因为过滤器的大小就是5*5，#
+# 所以和全连接层没有区别。如果将5*5*16矩阵中的节点拉成一个向量，那么这一层和全连接层就一样了。
+# 本层的输出节点个数为120，总共有5*5*16*120+120=48120个参数。
 model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+model.add(Dense(120, activation='relu'))
 
-model.compile(loss=keras.metrics.categorical_crossentropy,
-              optimizer=keras.optimizers.Adadelta(),
-              metrics=['accuracy'])
+# 本层的输入节点个数为120个，输出节点个数为84个，总共参数为120*84+84=10164个 (w + b)
+model.add(Dense(84, activation='relu'))
 
-model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs,
-          verbose=1, validation_data=(x_test, y_test))
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
+# 本层的输入节点个数为84个，输出节点个数为10个，总共参数为84*10+10=850
+model.add(Dense(10, activation='softmax'))
+
+model.compile(loss=keras.metrics.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+
+model.fit(x_train, y_train, batch_size=128, epochs=10, verbose=1, validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test)
+print('Test Loss:', score[0])
 print('Test accuracy:', score[1])
